44f60d198fc03b7ccc8cb535807d566adac5a102
Danh-Le <quang.danh.le@gmail.com>
2022-06-20 16:59:12 +0200

    test finished

---
+++ index.js
@@ -0,0 +1,59 @@
+// 01 - Numbers
+const integrer = 102
+const float = 13.9
+console.log(integrer, float)
+
+// 02 - Convert
+const basic = 34
+stringified = basic.toString()
+console.log(stringified.length)
+
+// 03 - Round
+const num = 1.5
+const rounded = Math.round(num)
+console.log(rounded)
+
+// 04 - Arithmétique
+const test = 12
+const bis = 5
+console.log(test+bis)
+console.log(test-bis)
+console.log(test*bis)
+console.log(test/bis)
+console.log(test%bis)
+
+// 05 - Reste division euclidienne
+const eucli = 999
+console.log(eucli%1)
+console.log(eucli%2)
+console.log(eucli%3)
+console.log(eucli%4)
+console.log(eucli%5)
+
+const euclid = 25
+console.log(euclid%1)
+console.log(euclid%2)
+console.log(euclid%3)
+console.log(euclid%4)
+console.log(euclid%5)
+
+const euclidi = 190280
+console.log(euclidi%1)
+console.log(euclidi%2)
+console.log(euclidi%3)
+console.log(euclidi%4)
+console.log(euclidi%5)
+
+const euclidie = 47
+console.log(euclidie%1)
+console.log(euclidie%2)
+console.log(euclidie%3)
+console.log(euclidie%4)
+console.log(euclidie%5)
+
+const euclidien = 691
+console.log(euclidien%1)
+console.log(euclidien%2)
+console.log(euclidien%3)
+console.log(euclidien%4)
+console.log(euclidien%5)
